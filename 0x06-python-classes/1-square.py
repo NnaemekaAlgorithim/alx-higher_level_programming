dfa12a479a4baf927f14ce54783611f2c473e6d1
NnaemekaAlgorithim <nnaemekanwodo625@gmail.com>
2023-01-26 00:22:57 +0000

    learning OOP

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,10 +4,8 @@
 
 class Square:
     """Represent a square."""
-
     def __init__(self, size):
         """Initialize a new Square.
-
         Args:
             size (int): The size of the new square.
         """
