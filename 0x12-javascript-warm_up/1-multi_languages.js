7960e53ca5b9543a48a0d1d16c85ffbce971a568
NnaemekaAlgorithim <nnaemekanwodo625@gmail.com>
2023-04-12 06:32:49 +0100

    corrected speling error

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // This JS code prints three lines using a single console.log function
 
-console.log('C is fun\nPython is cool\nJavaScript is amaizing');
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
