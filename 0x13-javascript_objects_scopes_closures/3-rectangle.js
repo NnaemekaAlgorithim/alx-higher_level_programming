4652943589fcc6e9c29ad5e57ad4c02b95e432b0
root <root@NWODO>
2023-04-12 23:53:20 +0100

    fixed semistandard errors

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-    constructor (w, h) {
-	if (w >= 1 && h >= 1){
-	    [this.width, this.height] = [w, h];
-	}
+  constructor (w, h) {
+    if (w >= 1 && h >= 1) {
+      [this.width, this.height] = [w, h];
     }
+  }
 
-    print () {
-	for (let j = 0; j < this.height; j++){
-	    console.log('X'.repeat(this.width));
-	}
+  print () {
+    for (let j = 0; j < this.height; j++) {
+      console.log('X'.repeat(this.width));
     }
+  }
 };
--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,15 +2,15 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-      if (w > 0 && h > 0) {
-	  [this.width, this.height] = [w, h];
-      }
+    if (w > 0 && h > 0) {
+      [this.width, this.height] = [w, h];
+    }
   }
 
   print () {
-      for (let j = 0; j < this.height; j++) {
-	  console.log('X'.repeat(this.width));
-      }
+    for (let j = 0; j < this.height; j++) {
+      console.log('X'.repeat(this.width));
+    }
   }
 
   double () {
