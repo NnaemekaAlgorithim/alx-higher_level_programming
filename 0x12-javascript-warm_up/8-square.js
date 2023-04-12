9d65a9942d4598d668b54f5194b7172b71e5d244
NnaemekaAlgorithim <nnaemekanwodo625@gmail.com>
2023-04-12 06:39:18 +0100

    i wrote wrong error message

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 // This code prints a square using X based on the value of argument passed
 
 if (isNaN(process.argv[2])) {
-  console.log('Missing value of size');
+  console.log('Missing size');
 } else if (parseInt(process.argv[2]) > 0) {
   for (let j = 0; j < parseInt(process.argv[2]); j++) {
     console.log('X'.repeat(parseInt(process.argv[2])));
