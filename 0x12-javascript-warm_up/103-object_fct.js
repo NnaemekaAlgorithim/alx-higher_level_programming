626a257eb98b15fed63058ad29c3a9a426397843
NnaemekaAlgorithim <nnaemekanwodo625@gmail.com>
2023-04-12 05:34:41 +0100

    increments the integer value

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// This code increaments the variable value in myObject.
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
