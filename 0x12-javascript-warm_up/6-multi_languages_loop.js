fdcaa2de41c65d29c0d81308203fc48f62f48537
NnaemekaAlgorithim <nnaemekanwodo625@gmail.com>
2023-04-12 06:35:01 +0100

    corrected spelling error of amazing in code

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // This code prints lines using string array and loop.
 
-const str = ['C is fun', 'Python is cool', 'JavaScript is amaizing'];
+const str = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let x = 0; x < 3; x++) {
   console.log(str[x]);
