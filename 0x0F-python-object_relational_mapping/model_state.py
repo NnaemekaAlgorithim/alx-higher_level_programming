c156b5b719c63e5577ebfbd31bbffe4a2cea67aa
root <root@NWODO>
2023-04-17 22:21:12 +0100

    ignoring sql, using sqlalchemy

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,10 +2,11 @@
 # Defines a State model.
 # Inherits from SQLAlchemy Base and links to the MySQL table states.
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
 
 
 class State(Base):
