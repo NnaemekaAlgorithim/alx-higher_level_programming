007c79b5398e6895dc663eaeeb8fa2694b94c170
root <root@NWODO>
2023-04-17 23:47:27 +0100

    uodated the documentation

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -11,7 +11,12 @@ Base = declarative_base()
 
 
 class City(Base):
-
+    """Represents a city for a MySQL database.
+    Attributes:
+        id (str): The city's id.
+        name (sqlalchemy.Integer): The city's name.
+        state_id (sqlalchemy.String): The city's state id.
+    """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
