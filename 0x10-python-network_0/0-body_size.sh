18ae346c1da2d5074f4d64a06719f6a511c811ec
root <root@NWODO>
2023-04-27 23:00:55 +0100

    size of body from response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script to get the size  of the body response from request sent
+# Script to get the size in byte of the body response from request sent
 curl -so /dev/null -w '%{size_download}\n' $1
